0ad810139e17e3ddde4287d556783be609362ceb
Lê Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2023-10-21 20:29:30 +0700

    Update notes.java

--- notes.java
+++ notes.java
@@ -33,3 +33,7 @@ public boolean onTouchEvent(MotionEvent event) {
 
             startX = endX;
             startY = endY
+
+                Hello Mr. Cho, I have encountered an issue when performing the straightening operation with a quickly flung seek bar. The issue arises because the condition (degree >= 45F || degree <= -45F) is applied in the performStraighten(deltaDegree) function. In some cases, degree is calculated as getProgressFromState() - deltaDegree, and I believe this is incorrect. I attempted to change the subtraction to addition, but in some cases, when the seek bar is flung quickly, it still doesn't work correctly.
+
+This condition is causing the seek bar not to align with the preview in the straighten operation. You can observe this issue when moving the seek bar to 0; the preview does not match the original. If you don't mind, please review this commit. I'm unsure why these functions are needed. Thank you for taking the time to read this, Mr. Cho.
